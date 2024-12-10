9e189c6a0793377033e7b65e3ee38d12b4b14a11
Jan Matysik <jan.matysik19@gmail.com>
2024-12-10 12:31:41 +0100

    Zadanie1
    Stworzenie prostego programu Windows Forms.

---
+++ Zadanie1/Zadanie1/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace Zadanie1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.changeText = new System.Windows.Forms.Button();
+            this.enableCheckBox = new System.Windows.Forms.CheckBox();
+            this.labelToChange = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // changeText
+            // 
+            this.changeText.Location = new System.Drawing.Point(12, 12);
+            this.changeText.Name = "changeText";
+            this.changeText.Size = new System.Drawing.Size(140, 23);
+            this.changeText.TabIndex = 0;
+            this.changeText.Text = "Kliknięcie zmienia etykietę";
+            this.changeText.UseVisualStyleBackColor = true;
+            this.changeText.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // enableCheckBox
+            // 
+            this.enableCheckBox.AutoSize = true;
+            this.enableCheckBox.Checked = true;
+            this.enableCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.enableCheckBox.Location = new System.Drawing.Point(333, 12);
+            this.enableCheckBox.Name = "enableCheckBox";
+            this.enableCheckBox.Size = new System.Drawing.Size(139, 17);
+            this.enableCheckBox.TabIndex = 1;
+            this.enableCheckBox.Text = "Włącza zmianę etykiety";
+            this.enableCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // labelToChange
+            // 
+            this.labelToChange.Location = new System.Drawing.Point(0, 59);
+            this.labelToChange.Name = "labelToChange";
+            this.labelToChange.Size = new System.Drawing.Size(485, 23);
+            this.labelToChange.TabIndex = 2;
+            this.labelToChange.Text = "Kliknij przycisk, aby zmienić tekst";
+            this.labelToChange.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelToChange.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 91);
+            this.Controls.Add(this.labelToChange);
+            this.Controls.Add(this.enableCheckBox);
+            this.Controls.Add(this.changeText);
+            this.Name = "Form1";
+            this.Text = "Moja aplikacja Windows";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button changeText;
+        private System.Windows.Forms.CheckBox enableCheckBox;
+        private System.Windows.Forms.Label labelToChange;
+    }
+}
+
---
+++ Zadanie1/Zadanie1/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zadanie1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (enableCheckBox.Checked == true)
+            {
+                if(labelToChange.Text == "Z prawej")
+                {
+                    labelToChange.Text = "Z lewej";
+                    labelToChange.TextAlign = ContentAlignment.MiddleLeft;
+                }
+                else
+                {
+                    labelToChange.Text = "Z prawej";
+                    labelToChange.TextAlign = ContentAlignment.MiddleRight;
+                }
+            } 
+            else
+            {
+                labelToChange.Text = "Moźliwość zmiany tekstu została wyłączona";
+                labelToChange.TextAlign = ContentAlignment.MiddleCenter;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
