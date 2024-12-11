7c8e5e719b8766b8531f862f624d163eeab95cab
Jan <jan.matysik1@gmail.com>
2024-12-11 09:11:40 +0100

    Zadanie7
    Wykorzystanie losowości do generowania menu baru.

---
+++ Zadanie7/Zadanie7/MenuMaker.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie7
+{
+    class MenuMaker
+    {
+        public Random Randomizer;
+
+        string[] Meats = { "Pieczona wołowina", "Salami", "Indyk", "Szynka", "Karkówka" };
+        string[] Condiments = {"żółta musztarda", "brązowa musztarda", "musztarda miodowa", "majonez", "przyprawa", "sos francuski"};
+
+        string[] Breads = {"chleb ryżowy", "chleb biały", "chleb zbożowy", "pumpernikiel", "chleb włoski", "bułka"};
+
+        public MenuMaker()
+        {
+            Randomizer = new Random();
+        }
+
+        public string GetMenuItem()
+        {
+            string randomMeat = Meats[Randomizer.Next(Meats.Length)];
+            string randomCondiments = Condiments[Randomizer.Next(Condiments.Length)];
+            string randomBread = Breads[Randomizer.Next(Breads.Length)];
+
+            return randomMeat + ", " + randomCondiments + ", " + randomBread;
+        }
+    }
+}
