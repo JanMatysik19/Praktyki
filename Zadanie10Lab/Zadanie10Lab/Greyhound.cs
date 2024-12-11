c84ed2a5a4bc07538f89ae03676903e5cfdc34a5
Jan <jan.matysik1@gmail.com>
2024-12-11 11:36:19 +0100

    Zadanie10Lab
    Utworzenie projektu zadanie10Lab.

---
+++ Zadanie10Lab/Zadanie10Lab/Greyhound.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zadanie10Lab
+{
+    class Greyhound
+    {
+        public int StartingPosition;
+        public int RaceTrackLength;
+        public PictureBox MyPictureBox;
+        public int Location = 0;
+        public Random MyRandom;
+
+
+
+        public bool Run()
+        {
+
+        }
+
+        public void TakeStartingPosition()
+        {
+
+        }
+
+    }
+}
