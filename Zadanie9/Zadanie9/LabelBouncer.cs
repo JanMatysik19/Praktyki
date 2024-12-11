ffbebfd3a30e9adc84cc57112266c93fe2cf0e2f
Jan <jan.matysik1@gmail.com>
2024-12-11 11:17:48 +0100

    Zadanie9
    Ta prosta aplikacja Windows Forms przedstawia animację poruszających się labeli.

---
+++ Zadanie9/Zadanie9/LabelBouncer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zadanie9
+{
+    class LabelBouncer
+    {
+        public Label MyLabel;
+        public bool GoingForword;
+
+        public LabelBouncer(Label MyLabel = null)
+        {
+            GoingForword = true;
+            this.MyLabel = MyLabel;
+        }
+
+        public void Move()
+        {
+            if(MyLabel != null)
+            {
+                if(GoingForword)
+                {
+                    MyLabel.Left += 5;
+
+                    if(MyLabel.Left >= MyLabel.Parent.Width - MyLabel.Width)
+                    {
+                        GoingForword = false;
+                    }
+                }
+                else
+                {
+                    MyLabel.Left -= 5;
+
+                    if(MyLabel.Left <= 0)
+                    {
+                        GoingForword = true;
+                    }
+                }
+            }
+        }
+    }
+}
