4003d72449cd1c5967167789f60a3f3d2eb155d0
Jan <jan.matysik19@gmail.com>
2024-12-12 08:33:44 +0100

    Zadanie12
    Prosty kalkulator paszy dla krów w Windows Forms. Przedstawia hermetyzację.

---
+++ Zadanie12/Zadanie12/Farmer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie12
+{
+    public class Farmer
+    {
+        public int BagsOfFeed {
+            get {
+                return NumberOfCows * FeedMultiplier;
+            }
+        }
+
+        
+        private int feedMultiplier;
+        public int FeedMultiplier {
+            get {
+                return feedMultiplier;
+            }
+        }
+
+
+        private int numberOfCows;
+        public int NumberOfCows
+        {
+            get {
+                return numberOfCows;
+            }
+            set {
+                numberOfCows = value;
+            }
+        }
+
+
+        public Farmer(int NumberOfCows = 0, int feedMultiplier = 30)
+        {
+            this.feedMultiplier = feedMultiplier;
+            numberOfCows = NumberOfCows;
+        }
+    }
+}
