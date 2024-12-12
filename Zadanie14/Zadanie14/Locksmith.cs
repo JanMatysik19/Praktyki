bb6e0472fec553b1876ffc754580d95f36f7c5f2
Jan <jan.matysik19@gmail.com>
2024-12-12 10:26:10 +0100

    Zadanie14
    Przedstawia to prostą aplikację konsolową, w której prezentuje się działanie przysłaniania metod.

---
+++ Zadanie14/Zadanie14/Locksmith.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie14
+{
+    class Locksmith
+    {
+        public void OpenSafe(Safe safe, Owner owner)
+        {
+            safe.PickLock(this);
+            Jewels safeContents = safe.Open(writtenDownCombination);
+            ReturnContents(safeContents, owner);
+        }
+
+        private string writtenDownCombination = null;
+        public void WrtieDownCombination(string combination)
+        {
+            writtenDownCombination = combination;
+        }
+
+        public virtual void ReturnContents(Jewels safeContents, Owner owner)
+        {
+            owner.ReciveContents(safeContents);
+        }
+    }
+}
