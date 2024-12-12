bbc93d955eb70cb4543a134ac8155d652ff7edff
Jan <jan.matysik19@gmail.com>
2024-12-12 14:29:28 +0100

    Zadani18
    Stworzenie aplikacji konsolowej przedstawiającej zastosowanie interfejsów.

---
+++ Zadanie18/Zadanie18/IClown.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie18
+{
+    interface IClown
+    {
+        void Honk();
+
+        string FunnyThingIHave {  get; }
+
+    }
+}
