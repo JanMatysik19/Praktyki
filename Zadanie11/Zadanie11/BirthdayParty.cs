03bddf149a0514f37b55985730dd4029b69fbb33
Jan <jan.matysik19@gmail.com>
2024-12-12 11:10:47 +0100

    Zadanie15
    Wprowadzenie dziedziczenia do aplikacji z zadania 11.

--- Zadanie11/Zadanie11/BirthdayParty.cs
+++ Zadanie11/Zadanie11/BirthdayParty.cs
@@ -6,12 +6,23 @@ using System.Threading.Tasks;
 
 namespace Zadanie11
 {
-    class BirthdayParty
+    class BirthdayParty : Party
     {
-        public const int CostOfFoodPerPerson = 25;
-        public int NumberOfPeople { get; set; }
-        public bool FancyDecorations {  get; set; }
         public string CakeWriting { get; set; }
+
+        public override decimal Cost
+        {
+            get
+            {
+                decimal totalCost = base.Cost;
+
+                decimal cakeCost;
+                if (CakeSize() == 20) cakeCost = 40M + ActualLength * 0.25M;
+                else cakeCost = 75M + ActualLength * 0.25M;
+
+                return totalCost + cakeCost;
+            }
+        }
 
         public BirthdayParty(int numberOfPeople, bool fancyDecorations, string cakeWriting)
         {
@@ -36,30 +47,6 @@ namespace Zadanie11
             {
                 if (CakeWriting.Length > MaxWritingLength()) return true;
                 else return false;
-            }
-        }
-
-        private decimal CalculateCostOfDecorations()
-        {
-            decimal costOfDecorations;
-            if (FancyDecorations) costOfDecorations = (NumberOfPeople * 15.0M) + 50.0M;
-            else costOfDecorations = (NumberOfPeople * 7.5M) + 30.0M;
-            return costOfDecorations;
-        }
-
-        public decimal Cost
-        {
-            get
-            {
-                decimal totalCost = CalculateCostOfDecorations();
-                totalCost += CostOfFoodPerPerson * NumberOfPeople;
-                decimal cakeCost;
-                if (CakeSize() == 20) cakeCost = 40M + ActualLength * 0.25M;
-                else cakeCost = 75M + ActualLength * 0.25M;
-
-                if (NumberOfPeople > 12) totalCost += 100M;
-
-                return totalCost + cakeCost;
             }
         }
 
--- Zadanie11/Zadanie11/DinnerParty.cs
+++ Zadanie11/Zadanie11/DinnerParty.cs
@@ -8,106 +8,46 @@ using System.Windows.Forms;
 namespace Zadanie11
 {
 
-    class DinnerParty
+    class DinnerParty : Party
     {
-        private bool healtyOption;
-        public bool HealtyhOption
-        {
-            set
-            {
-                healtyOption = value;
-            }
-        }
-        
-        private bool fancyOption;
-        public bool FancyOption
-        {
-            set
-            {
-                fancyOption = value;
-            }
-        }
-
-
-        private int numberOfPeople;
-        public int NumerOfPeople
+        public bool HealthyOption {  get; set; }
+        override public decimal Cost
         {
             get
             {
-                return numberOfPeople;
-            }
-            set
-            {
-                numberOfPeople = value;
-            }
-        }
-        public decimal Cost
-        {
-            get
-            {
-                return CalculateCost();
-            }
-        }
+                decimal totalCost = base.Cost;
 
+                totalCost += NumberOfPeople * CostOfBeveragesPerPerson;
+
+
+                if(HealthyOption)
+                {
+                    totalCost *= 0.95M;
+                }
+                return totalCost;
+            }
+        }
 
         private decimal CostOfBeveragesPerPerson
         {
             get
             {
-                return SetHealthyOption(healtyOption);
-            }
-        }
-        private decimal CostOfDecorations
-        {
-            get
-            {
-                return CalculateCostOfDecorations(fancyOption);
+                decimal costOfBeveragesPerPerson;
+
+                if(HealthyOption) costOfBeveragesPerPerson = 5.0M;
+                else costOfBeveragesPerPerson = 20.0M;
+
+                return costOfBeveragesPerPerson;
             }
         }
         
-        
 
-
-
-        public DinnerParty(int NumerOfPeople = 10, bool healthyOption = false, bool fancyOption = true)
+        public DinnerParty(int numberOfPeople = 10, bool healthyOption = false, bool fancyOption = true)
         {
-            this.NumerOfPeople = NumerOfPeople;
-            this.fancyOption = fancyOption;
-            this.healtyOption = healthyOption;
+            NumberOfPeople = numberOfPeople;
+            FancyDecorations = fancyOption;
+            HealthyOption = healthyOption;
 
-            SetHealthyOption(healthyOption);
-            CalculateCostOfDecorations(fancyOption);
-
-        }
-
-        private decimal SetHealthyOption(bool value)
-        {
-            decimal costOfBeveragesPerPerson;
-
-            if(value) costOfBeveragesPerPerson = 5.0M;
-            else costOfBeveragesPerPerson = 20.0M;
-
-            return costOfBeveragesPerPerson;
-        }
-
-        private decimal CalculateCostOfDecorations(bool value)
-        {
-            decimal costOfDecorations;
-            if(value) costOfDecorations = 15.0M * numberOfPeople + 50.0M;
-            else costOfDecorations = 7.5M * numberOfPeople + 30.0M;
-
-            return costOfDecorations;
-        }
-
-        private decimal CalculateCost()
-        {
-            decimal cost = numberOfPeople * (25.0M + CostOfBeveragesPerPerson) + CostOfDecorations;
-
-            if (CostOfBeveragesPerPerson == 5.0M) cost *= 0.95M;
-
-            if (numberOfPeople > 12) cost += 100M;
-
-            return cost;
         }
     }
 }
