e38711f42947941017c97fcb152f843f9ce38985
Jan <jan.matysik19@gmail.com>
2024-12-13 14:57:43 +0100

    Zadanie26
    Zadanie do dwóch talii kart.
    Znane problemy:
    metoda Shuffle nie działa.

---
+++ Zadanie26/Zadanie26/Deck.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie26
+{
+    internal class Deck
+    {
+        private List<Card> cards;
+        private Random random = new Random();
+
+        public Deck()
+        {
+            cards = new List<Card>();
+            for(int suit = 0; suit < 4; suit++) for(int value = 1; value <= 13; value++) cards.Add(new Card(suit, value));
+        }
+
+        public Deck(IEnumerable<Card> initialCards)
+        {
+            cards = new List<Card>(initialCards);
+        }
+
+        public int Count
+        {
+            get
+            {
+                return cards.Count;
+            }
+        }
+
+        public void Add(Card card)
+        {
+            cards.Add(card);
+        }
+
+        public Card Deal(int index)
+        {
+            Card CardToDeal = cards[index];
+            cards.RemoveAt(index);
+            return CardToDeal;
+        }
+
+        public void Shuffle() {
+            foreach(Card card in cards)
+            {
+                int a = cards.IndexOf(card);
+                int b = random.Next(0, cards.Count);
+                
+                Card tmp = cards[a];
+                cards[a] = cards[b];
+                cards[b] = tmp;
+            }
+        }
+
+        private void swap(List<Card> list, int a, int b)
+        {
+            Card tmp = cards[a];
+            cards[a] = cards[b];
+            cards[b] = tmp;
+        }
+
+        public IEnumerable<string> GetCardNames()
+        {
+            List<string> tmp = new List<string>();
+            foreach(Card card in cards) tmp.Add(card.Name);
+            return tmp;
+        }
+
+        public void Sort()
+        {
+            cards.Sort(new CardComparer_bySuit());
+        }
+    }
+}
---
+++ Zadanie26/Zadanie26/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zadanie26
+{
+    public partial class Form1 : Form
+    {
+        Deck deck1;
+        Deck deck2;
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+            deck1 = new Deck();
+            ResetDeck(1);
+            RedrawDeck(1);
+
+            deck2 = new Deck();
+            ResetDeck(1);
+            RedrawDeck(2);
+        }
+
+        private void ResetDeck(int deck)
+        {
+            Random random = new Random();
+            List<Card> cards = new List<Card>();
+
+            if (deck == 1)
+            {
+                for (int i = 1; i <= 10; i++) cards.Add(new Card(random.Next(0, 4), random.Next(1, 14)));
+
+                deck1 = new Deck(cards);
+                deck1.Sort();
+                RedrawDeck(1);
+            }
+            else if (deck == 2)
+            {
+                for (int suit = 0; suit < 4; suit++) for (int value = 1; value <= 13; value++) cards.Add(new Card(suit, value));
+
+                deck2 = new Deck(cards);
+                deck2.Sort();
+                RedrawDeck(2);
+            }
+            else MessageBox.Show("Nieznany zestaw!");
+        }
+
+        private void RedrawDeck(int deck)
+        {
+            if (deck == 1)
+            {
+                listBox1.Items.Clear();
+                foreach (string name in deck1.GetCardNames()) listBox1.Items.Add(name);
+
+                label1.Text = "Zestaw 1. (" + deck1.Count + " kart)";
+            }
+            else if (deck == 2)
+            {
+                listBox2.Items.Clear();
+                foreach (string name in deck2.GetCardNames()) listBox2.Items.Add(name);
+                label2.Text = "Zestaw 2. (" + deck2.Count + " kart)";
+            }
+            else MessageBox.Show("Nieznany zestaw!");
+        }
+
+        private void reset1_Click(object sender, EventArgs e)
+        {
+            ResetDeck(1);
+            RedrawDeck(1);
+        }
+
+        private void reset2_Click(object sender, EventArgs e)
+        {
+            ResetDeck(2);
+            RedrawDeck(2);
+        }
+
+        private void shuffle1_Click(object sender, EventArgs e)
+        {
+            deck1.Shuffle();
+            RedrawDeck(1);
+        }
+
+        private void shuffle2_Click(object sender, EventArgs e)
+        {
+            deck2.Shuffle();
+            RedrawDeck(2);
+        }
+    }
+}
