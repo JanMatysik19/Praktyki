f6d3316347c21656dd994a94bbf5f622abe87e42
Jan <jan.matysik19@gmail.com>
2025-01-13 13:48:08 +0100

    Zadanie36
    Przedstawienie działania delegatów

---
+++ Zadanie36/Zadanie36/Amy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie36
+{
+    internal class Amy
+    {
+        public GetSecretIndegrient MySecretIndegrientMethod
+        {
+            get { return new GetSecretIndegrient(AmysSecretIndegrient); }
+        }
+
+
+        private string AmysSecretIndegrient(int amount)
+        {
+            if (amount < 10) return amount.ToString() + " puszek sardynek -- potrzepujesz więcej!";
+            else return amount.ToString() + " puszek sardynek";
+        }
+    }
+}
