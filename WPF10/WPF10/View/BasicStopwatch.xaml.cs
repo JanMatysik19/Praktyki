b3e7f0432ca08db020cbfb96378c10abb0901a6a
Jan <jan.matysik19@gmail.com>
2025-01-14 14:54:38 +0100

    WPF10
    Aplikacja stopera.
    -Ikonki windows 8 nie działają. Nawet nie ma co się nad tym dłużej zastanawiać.
    - Animacja nie działa. Rozwiązanie: zrobić template guzika i zmieniać właściwości parametrów.

---
+++ WPF10/WPF10/View/BasicStopwatch.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPF10.ViewModel;
+
+namespace WPF10.View
+{
+    /// <summary>
+    /// Logika interakcji dla klasy BasicStopwatch.xaml
+    /// </summary>
+    public partial class BasicStopwatch : UserControl
+    {
+        StopwatchViewModel viewModel;
+
+        public BasicStopwatch()
+        {
+            InitializeComponent();
+
+            viewModel = new StopwatchViewModel();
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            viewModel.Start();
+        }
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            viewModel.Stop();
+        }
+
+        private void ResetBtton_Click(object sender, RoutedEventArgs e)
+        {
+            viewModel.Reset();
+        }
+
+        private void LapBtton_Click(object sender, RoutedEventArgs e)
+        {
+            viewModel.Lap();
+        }
+    }
+}
