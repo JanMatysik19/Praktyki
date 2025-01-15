da18b49e7586cfdd5e9c55788ff61f7399a54066
Jan <jan.matysik19@gmail.com>
2025-01-15 11:16:50 +0100

    WPF10
    - Naprawiono wszystkie niesprawności
    - Dodano zegar analogowy
    - Dodano template buttona

--- WPF10/WPF10/View/AnalogStopWatch.xaml.cs
+++ WPF10/WPF10/View/AnalogStopWatch.xaml.cs
@@ -17,16 +17,38 @@ using WPF10.ViewModel;
 namespace WPF10.View
 {
     /// <summary>
-    /// Logika interakcji dla klasy BasicStopwatch.xaml
+    /// Logika interakcji dla klasy AnalogStopWatch.xaml
     /// </summary>
-    public partial class BasicStopwatch : UserControl
+    public partial class AnalogStopWatch : UserControl
     {
         StopwatchViewModel viewModel;
 
-        public BasicStopwatch()
+        public AnalogStopWatch()
         {
             InitializeComponent();
             viewModel = this.Resources["viewModel"] as StopwatchViewModel;
+
+            AddMarking();
+        }
+
+
+        private void AddMarking()
+        {
+            for(int i = 0; i < 360; i +=3)
+            {
+                Rectangle rectangle = new Rectangle();
+                rectangle.Width = (i % 30 == 0) ? 3 : 1;
+                rectangle.Height = 15;
+                rectangle.Fill = new SolidColorBrush(Colors.Black);
+                rectangle.RenderTransformOrigin = new Point(0.5, 0.5);
+
+                TransformGroup transforms = new TransformGroup();
+                transforms.Children.Add(new TranslateTransform() { Y = -140 });
+                transforms.Children.Add(new RotateTransform() { Angle = i });
+                rectangle.RenderTransform = transforms;
+
+                baseGrid.Children.Add(rectangle);
+            }
         }
 
         private void StartButton_Click(object sender, RoutedEventArgs e)
